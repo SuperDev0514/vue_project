054581bd7980b60fb34f051f18351ceb628361ea
SuperDev0514 <upretysunil49@gmail.com>
2020-04-22 18:22:15 +0200

    Clicked X times button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Clicked {{ timesClicked }} times.</h1>
+    <button @click="iAmClicked()">Click me!</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    data() {
+      return {timesClicked: 0}
+    },
+    methods: {
+      iAmClicked() {
+        this.timesClicked += 1;
+      }
+    }
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
