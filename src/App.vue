0e2761e29f275fd0372e20b50f2cffcc58c825fb
SuperDev0514 <upretysunil49@gmail.com>
2020-04-25 10:05:24 +0200

    second task completed

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <h1>Twój e-mail to {{ email }}</h1>
-    <h3>e-mail: {{ email }}</h3>
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
     <input type="email" v-model="email">
 
   </div>
